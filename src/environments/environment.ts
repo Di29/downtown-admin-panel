f7c6b961925390ea44a0ee9bd9e61e13d44d36cc
Urinbasarov Dias <jentle.men29@gmail.com>
2020-10-07 00:25:01 +0600

    prod true

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:9966/'
+  production: true,
+  //apiUrl: 'http://localhost:9966/'
+  apiUrl: 'https://downtown-bot-api.herokuapp.com/'
 
 };
 
